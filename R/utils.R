9be5482dcce64e8b07a9ae845a7bbc66d611bc9a
RLumSK <sebastian.kreutzer@aber.ac.uk>
2022-04-04 19:14:50 +0100

    Add function to convert from numeric to the needed character values

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
-#'@title Fast Conversion of Comma Separated Strings to Numeric
+#'@title Fast Conversion of White Space Separated Strings to Numeric
 #'
-#'@description Converts strings with comma separated numbers to a numeric vector. `"NA"`
+#'@description Converts strings with white space separated numbers to a numeric vector. `"0"`
 #'values are transformed to `NA`.
 #'
 #'@param l [character] [list] (**required**): list of character vectors (see example)
@@ -26,6 +26,32 @@
       lapply(l, strsplit, split = " ", fixed = TRUE),
       recursive = FALSE),
     as.numeric)
+}
+
+#'@title Fast Conversion of Numeric Values into White Space Separated Strings
+#'
+#'@description Converts numeric vector to a character with numbers separated by white space. `NA`
+#'values are transformed to `"0"`.
+#'
+#'@param l [numeric] [list] (**required**): list of numeric vectors (see example)
+#'
+#'@author Sebastian Kreutzer, Geography & Earth Sciences, Aberystwyth University (United Kingdom)
+#'
+#'@return [list] of [numeric] vectors
+#'
+#'@examples
+#'
+#'l = list(a <- c(12, 12, 13), b = NA)
+#'.convert2character(l)
+#'
+#'@md
+#'@noRd
+.convert2character <- function(l){
+  ## handle NA values
+  if (any(is.na(l))) l[which(is.na(l))] <- "0"
+
+  ##extract values
+  lapply(l, function(x) paste(as.character(x), collapse = " "))
 }
 
 #'@title Get Names-based Element Index from Nested list

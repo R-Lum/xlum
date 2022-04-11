21dbd3ee2c139610c1f9f7fa0d95e01befe1e964
RLumSK <sebastian.kreutzer@aber.ac.uk>
2022-04-11 14:48:01 +0100

    Add convert_xsyg2xlum() + tests

---
+++ R/convert_xsyg2xlum.R
@@ -0,0 +1,38 @@
+#'@title Convert Freiberg Instruments XSYG files to XLUM
+#'
+#'@decription Wrapped function to quickly convert Daybreak files
+#'to `xlum` (files)
+#'
+#'@param file [character] (**required**): file
+#'
+#'@param out_file [character] (*optional*): output file, if set a file
+#'output is created
+#'
+#'@author Sebastian Kreutzer, Geography & Earth Sciences, Aberystwyth University (United Kingdom)
+#'
+#'@section Function version: 0.1.0
+#'
+#'@return Depending on the setting. If `out_file == NULL` an [xml2::xml_document-class] object is returned,
+#' if file is set the function attempts to write an `*.xlum` file`.
+#'
+#'
+#'@seealso [convert_rlum2xlum], [Luminescence::read_XSYG2R]
+#'
+#'@examples
+#'file <- system.file("extdata/XSYG_file.xsyg", package = "Luminescence")
+#'convert_xsyg2xlum(file)
+#'
+#'@md
+#'@export
+convert_xsyg2xlum <- function(
+    file,
+    out_file = NULL
+){
+  rlum <-
+    Luminescence::read_XSYG2R(
+      file = file,
+      fastForward = TRUE,
+      verbose = FALSE)
+  convert_rlum2xlum(rlum, out_file)
+}
+
---
+++ tests/testthat/test_convert_xsyg2xlum.R
@@ -0,0 +1,10 @@
+test_that("convert_xsyg2xlum()", {
+  testthat::skip_on_cran()
+  local_edition(3)
+
+  ## BINX
+  file <- system.file("extdata/XSYG_file.xsyg", package = "Luminescence")
+  testthat::expect_s3_class(convert_xsyg2xlum(file), "xml_document")
+
+
+})

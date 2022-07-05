b1566611cf968f31b6d0fc54e749988b3d157555
RLumSK <sebastian.kreutzer@uni-heidelberg.de>
2022-07-05 09:41:36 +0200

    validate_xlum(): add support for versioning of XSD aka XLUM file formats

--- R/validate_xlum.R
+++ R/validate_xlum.R
@@ -5,6 +5,9 @@
 #'
 #'@param file [xml2::xml_document-class] (**required**): object to test
 #'
+#'@param xsd_version [numeric] (*optional*): specify format version for the
+#'validation, e.g., `1`, `1.1`. The default is `NULL`, which will use the most recent version
+#'
 #'@return Results of the validation [logical] with attributes
 #'
 #'@author Sebastian Kreutzer, Geography & Earth Sciences, Aberystwyth University
@@ -15,15 +18,41 @@
 #'
 #'@md
 #'@export
-validate_xlum <- function(file){
+validate_xlum <- function(
+    file,
+    xsd_version = NULL
+){
   ## fall back
   if (!inherits(file, "xml_document"))
     file <- xml2::read_xml(file)
 
+  ## load validation schema
+  ## load all schema files
+  f_schema <- sort(list.files(
+    path = system.file("extdata/", package = "xlum"),
+    full.names = TRUE,
+    pattern = "xlum\\_schema"),
+    decreasing = TRUE)
+
+    ## use different schema
+    if(!is.null(xsd_version)) {
+      if(!any(xsd_version[1] %in% .regmatches(x = basename(f_schema), match = "(?<=v).+(?=.xsd)")))
+        stop(
+          paste("[validate_xlum()] XSD version unknown. Known versions:",
+                paste(
+                  .regmatches(x = basename(f_schema), match = "(?<=v).+(?=.xsd)"),
+                collapse = ", ")
+                ),
+          call. = FALSE)
+
+      tmp <- f_schema[grepl(pattern = xsd_version[1], x = f_schema, fixed = TRUE)]
+
+    }
+
   ## validate schema
   schema_check <- xml2::xml_validate(
     x = file,
-    schema = xml2::read_xml(system.file("extdata/xlum_schema.xsd", package = "xlum")))
+    schema = xml2::read_xml(f_schema[1]))
 
   ## return results
   if (!all(schema_check))
--- tests/testthat/test_validate_xlum.R
+++ tests/testthat/test_validate_xlum.R
@@ -6,6 +6,13 @@ test_that("validate_xlum()", {
   file <- system.file("extdata/xlum_prototype.xlum", package = "xlum")
   expect_true(validate_xlum(file))
 
+  ## use different version
+  expect_true(validate_xlum(file, xsd_version = 1))
+
+  ## trigger error
+  expect_error(expect_true(validate_xlum(file, xsd_version = 9.999)),
+               regexp = "\\[validate\\_xlum\\(\\)\\] XSD version unknown")
+
   ## check broken prototype
   file_invalid <- system.file("extdata/xlum_invalid.xlum", package = "xlum")
   expect_false(suppressWarnings(validate_xlum(file_invalid)))
